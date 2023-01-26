acd64848c555dc78d0b5c46a2b2785211bba79f9
Dima <dbuyvid@gmail.com>
2023-01-26 16:39:57 +0300

    Added structures for jwt and user

---
+++ pkg/domain/auth.go
@@ -0,0 +1,20 @@
+package domain
+
+import "github.com/dgrijalva/jwt-go"
+
+type JwtTokens struct {
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
+}
+
+type UserCredentials struct {
+	Username     string `json:"username,omitempty"`
+	MobileNumber string `json:"mobileNumber,omitempty"`
+	Password     string `json:"password,required"`
+}
+
+type JwtClaims struct {
+	jwt.StandardClaims
+	UserId   int    `json:"userId,required"`
+	Username string `json:"username,required"`
+}
