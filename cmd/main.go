01dbe5f06953c1a236d63069a61ce9dfcf503ffb
Dima <dbuyvid@gmail.com>
2023-01-27 15:18:43 +0300

    Replaced path to folder

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"log"
 
-	"github.com/Semaffor/go__innotaxi_service_user/pkg/server"
+	"github.com/Semaffor/go__innotaxi_service_user/pkg/app"
 )
 
 const configsDir = "pkg/config/fixture"
 
 func main() {
-	if err := server.Run(configsDir); err != nil {
+	if err := app.Run(configsDir); err != nil {
 		log.Fatalf("%v\n", err)
 	}
 }
