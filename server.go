0a046583acba62c40ebd1c241c48c27e6292d907
Dima <dbuyvid@gmail.com>
2023-01-27 15:16:51 +0300

    Modified input params in func

--- server.go
+++ server.go
@@ -4,15 +4,17 @@ import (
 	"context"
 	"net/http"
 	"time"
+
+	"github.com/Semaffor/go__innotaxi_service_user/pkg/config"
 )
 
 type Server struct {
 	httpServer *http.Server
 }
 
-func (s *Server) Run(port string, handler http.Handler) error {
+func (s *Server) Run(serverConfig *config.Server, handler http.Handler) error {
 	s.httpServer = &http.Server{
-		Addr:           ":" + port,
+		Addr:           ":" + serverConfig.Port,
 		Handler:        handler,
 		MaxHeaderBytes: 1 << 20, // 1 MB
 		ReadTimeout:    10 * time.Second,
