917574a4e6fe3f1b0507deb65fccba19d261d460
Dima <dbuyvid@gmail.com>
2023-02-02 16:55:09 +0300

    Updated services

--- pkg/service/postgres/service.go
+++ pkg/service/postgres/service.go
@@ -1,10 +1,12 @@
 package postgres
 
 import (
+	"github.com/Semaffor/go__innotaxi_service_user/pkg/domain"
 	repo "github.com/Semaffor/go__innotaxi_service_user/pkg/repository/postgres"
 )
 
 type User interface {
+	Authentication(credentials *domain.UserCredentials) (domain.User, error)
 }
 
 type ServicePostgres struct {
--- pkg/service/postgres/userService.go
+++ pkg/service/postgres/userService.go
@@ -1,8 +1,17 @@
 package postgres
 
 import (
+	"github.com/Semaffor/go__innotaxi_service_user/pkg/domain"
 	"github.com/Semaffor/go__innotaxi_service_user/pkg/repository/postgres"
 )
+
+var fakeUserStruct = &domain.User{
+	Id:           1,
+	Username:     "Dima",
+	PhoneNumber:  "111",
+	Email:        "dd@mail.ru",
+	PasswordHash: "1",
+}
 
 type UserService struct {
 	repo *postgres.UserRepository
@@ -10,4 +19,9 @@ type UserService struct {
 
 func NewUserService(repo *postgres.UserRepository) *UserService {
 	return &UserService{repo: repo}
+}
+
+func (u *UserService) Authentication(credentials *domain.UserCredentials) (domain.User, error) {
+	// find user in db
+	return *fakeUserStruct, nil
 }
