0a8242c1fde660016b2a442a4a2550228533af04
Dima <dbuyvid@gmail.com>
2023-02-07 18:40:00 +0300

    Connected mongo via docker, implements basic functionality

--- pkg/repository/mongo/mongo.go
+++ pkg/repository/mongo/mongo.go
@@ -1,12 +1,36 @@
 package mongo
 
 import (
-	"github.com/jmoiron/sqlx"
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 
 	"github.com/Semaffor/go__innotaxi_service_user/pkg/config"
 )
 
-func NewConnection(config *config.ConfigDB) *sqlx.DB {
-	// connection to db
-	return nil
+func NewConnection(config *config.ConfigDB) *mongo.Database {
+	addr := net.JoinHostPort(config.Host, config.Port)
+	uri := fmt.Sprintf("mongodb://%s:%s@%s/", config.Username, config.Password, addr)
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatalf("Can't connect to mongoDB: %s", err.Error())
+	}
+
+	if err := client.Ping(context.Background(), readpref.Primary()); err != nil {
+		log.Fatalf("Can't connect to mongoDB: %s", err.Error())
+	}
+	db := client.Database(config.DBName)
+
+	return db
+}
+
+func shutdown(client *mongo.Client) {
+	if err := client.Disconnect(context.Background()); err != nil {
+		panic(err)
+	}
 }
