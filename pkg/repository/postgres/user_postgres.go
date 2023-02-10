f08259ca079909057f3b0d9b96fd2da5265cbfbf
Dima <dbuyvid@gmail.com>
2023-02-10 11:55:04 +0300

    Replaced on dao

--- pkg/repository/postgres/user_postgres.go
+++ pkg/repository/postgres/user_postgres.go
@@ -2,34 +2,36 @@ package postgres
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/jmoiron/sqlx"
 
+	"github.com/Semaffor/go__innotaxi_service_user/pkg/repository/postgres/dao"
+	"github.com/Semaffor/go__innotaxi_service_user/pkg/repository/postgres/general"
 	"github.com/Semaffor/go__innotaxi_service_user/pkg/repository/postgres/model"
 )
 
 type UserRepository struct {
-	db *sqlx.DB
+	db      *sqlx.DB
+	userDao general.DaoI[model.User]
 }
 
 func NewUserRepository(db *sqlx.DB) *UserRepository {
 	return &UserRepository{
-		db: db,
+		db:      db,
+		userDao: dao.NewUserDao(db, usersTable),
 	}
 }
 
 func (r *UserRepository) Save(ctx context.Context, user *model.User) (int, error) {
-	query := fmt.Sprintf("insert into %s (name, phone_number, email, password_hash, role)"+
-		" values ($1, $2, $3, $4, $5) returning id;", usersTable)
-	row := r.db.QueryRowx(query, user.Name, user.PhoneNumber, user.Email, user.PasswordHash, model.USER)
-
-	var id int
-	if err := row.Scan(&id); err != nil {
-		return 0, err
+	params := map[string]interface{}{
+		"name":          user.Name,
+		"phone_number":  user.PhoneNumber,
+		"email":         user.Email,
+		"password_hash": user.PasswordHash,
+		"role":          model.USER,
 	}
 
-	return id, nil
+	return r.userDao.Save(params)
 }
 
 func (r *UserRepository) Update(ctx context.Context, user *model.User) error {
@@ -59,46 +61,17 @@ func (r *UserRepository) Update(ctx context.Context, user *model.User) error {
 		params["password_hash"] = user.PasswordHash
 	}
 
-	query, args := generateUpdateQuery(usersTable, params)
-	query = fmt.Sprintf("%s WHERE id=%d", query, user.Id)
-	err := executeQuery(r.db, query, args)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return r.userDao.Update(params, user.Id)
 }
 
 func (r *UserRepository) DeleteUserById(ctx context.Context, userId int) error {
-	query := fmt.Sprintf("UPDATE %s SET is_deleted=true WHERE id=%d", usersTable, userId)
-
-	err := executeQuery(r.db, query, nil)
-	if err != nil {
-		return err
+	params := map[string]interface{}{
+		"is_deleted": true,
 	}
 
-	return nil
+	return r.userDao.Update(params, userId)
 }
 
-// FindByFields probably make generic.
-func (r *UserRepository) FindByFields(ctx context.Context, params map[string]interface{}) ([]model.User, error) {
-	var err error
-	query, args := generateSelectQuery(usersTable, params)
-
-	var users []model.User
-	if args != nil {
-		err = r.db.Select(&users, query, args...)
-	} else {
-		err = r.db.Select(&users, query)
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	return users, nil
-}
-
-// FindAll probably make generic.
 func (r *UserRepository) FindAll(ctx context.Context) ([]model.User, error) {
-	return r.FindByFields(ctx, nil)
+	return r.userDao.FindByFields([]model.User{}, nil)
 }
